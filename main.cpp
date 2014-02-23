b183b882c6b1796a1e414389656fcf065c07f0ba
mattlarsen0 <matt.larsen0@gmail.com>
2014-02-23 13:11:08 -0800

    more work on tracking

--- main.cpp
+++ main.cpp
@@ -6,121 +6,143 @@
 #include <iostream>
 
 //#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+void detect();
+void track();
 
 int main()
 {
-	int answer;
-	std::cout << "1 for motion detection, 2 for motion tracking" << std::endl;
-	std::cin >> answer;
+	int answer =2;
+	//std::cout << "1 for motion detection, 2 for motion tracking" << std::endl;
+//	std::cin >> answer;
 
 	if(answer != 1 && answer != 2)
 		answer = 2;
 
-
-	cv::VideoCapture vid;
-	cv::Mat prevFrame;
-	cv::Mat currFrame;
-	std::vector<std::vector<cv::Point>> contours;
-	cv::BackgroundSubtractorMOG2 subtractor(0, 12, false);
-	cv::Scalar color = cv::Scalar(0,255,0);
-	cv::Mat mask; //Mask results from MOG2 subtraction
-	cv::Mat temp; //Temp image for manipulation of current frame
-	
-	vid.open("test.avi");
-	
 	if(answer == 1)
-	{	
-		while(vid.read(currFrame) == true)
-		{
-			temp = currFrame.clone();
-			cv::blur(temp, temp, cv::Size(9,9));
-			cv::threshold(temp, temp, 180, 255, CV_THRESH_TRUNC);
-
-			cv::Rect boundRect;
-
-			subtractor.operator()(temp, mask);
-				
-			cv::imshow("Background Subtraction", mask);
-			//motion analysis and tracking, template
-
-			cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
-			//Draw rectangles
-			for(size_t i = 0; i < contours.size(); i++)
-			{
-				cv::Moments moment = moments((cv::Mat)contours[i]);
-				double area = moment.m00;
-
-				if((area > 80))
-				{
-					//std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
-					//cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
-					boundRect = cv::boundingRect(contours[i]);
-					rectangle(currFrame, boundRect.tl(), boundRect.br(), color, 2);	
-				}
-			}
-
-			cv::imshow("Result", currFrame);
-			cv::waitKey(1);
-		}
+	{
+		detect();
 	}
 	else if(answer == 2)
 	{
-		//Init first frame
-		//select features in first frame
-		cv::Mat prevDescriptors;
-		cv::Mat currDescriptors;
+		cv::VideoCapture vid;
 		std::vector<cv::Point2f> prevPoints;
 		std::vector<cv::Point2f> currPoints;
-		std::vector<cv::KeyPoint, std::allocator <cv::KeyPoint>> prevKey;
-		std::vector<cv::KeyPoint, std::allocator <cv::KeyPoint>> currKey;
-		cv::Mat currGray;
-		cv::Mat prevGray;
+		std::vector<cv::Point2f> searchPoints;
+		cv::Mat background;
+		std::vector<cv::Point2f> topPoints;
+		std::vector<cv::Point2f> bottomPoints;
+		std::vector<cv::Point2f> leftPoints;
+		cv::Rect boundRect;
+		std::vector<cv::Point2f> rightPoints;
+		cv::Mat mask;
+		cv::Mat prevMask;
+		cv::Mat currFrame;
+		cv::Mat prevFrame;
+		cv::Mat temp;
+		std::vector<std::vector<cv::Point>> contours;
+		std::vector<std::vector<cv::Point>> goodContours;
+		cv::Mat searchArea;
 		cv::Size winSize(10,10);
 		cv::Size zeroZone(-1,-1);
 		std::vector<uchar> status;
 	    std::vector<float> err;
 		
 		cv::TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03);
+		cv::BackgroundSubtractorMOG2 subtractor(50, 12, false);
+		
+		vid.open("test2.avi");
+
+
+		//do the first frame
+		vid.read(temp);
+
+		cv::blur(temp, temp, cv::Size(9,9));
+		subtractor.operator()(temp, mask);
 
 		vid.read(prevFrame);
-		cv::cvtColor(prevFrame, prevGray, CV_BGR2GRAY);
 
-		//detect features
-		cv::goodFeaturesToTrack(prevGray, prevPoints, 900, 0.01, 5);
-
-		//refine corners
-		cv::cornerSubPix(prevGray, prevPoints, winSize, zeroZone, termcrit);
-
-		prevGray.copyTo(currGray);
+		cv::cvtColor(prevFrame, prevFrame, CV_RGB2GRAY);	
+		cv::blur(prevFrame, prevFrame, cv::Size(9,9));
+		int frame=120;
 		
-
-		cv::calcOpticalFlowPyrLK(prevGray, currGray, prevPoints, currPoints, status, err, winSize, 3, termcrit);
-
-
-		
-		while(vid.read(currFrame))
+		while(vid.read(currFrame) == true)
 		{
-			cv::cvtColor(currFrame, currGray, CV_BGR2GRAY);
-			
+			temp = currFrame.clone();
+
+			cv::cvtColor(temp, temp, CV_RGB2GRAY);	
+			cv::blur(temp, temp, cv::Size(9,9));
+
+			if(frame == 120)
+			{
+				cv::goodFeaturesToTrack(prevFrame, prevPoints, 1000, 0.01, 3);
+
+				cv::cornerSubPix(prevFrame, prevPoints, winSize, zeroZone, termcrit);
+				frame = 0;
+			}
+
+			frame++;
+
 			//create optical flow pyramid
-			calcOpticalFlowPyrLK(prevGray, currGray, prevPoints, currPoints, status, err, winSize, 3, termcrit);
+			cv::calcOpticalFlowPyrLK(prevFrame, temp, prevPoints, currPoints, status, err, winSize, 3, termcrit);
 
+			prevFrame = temp.clone();
+			cv::Mat kernel = cv::Mat::ones(3,3, CV_32F);
+
+			//find and draw rectangles around movement
+			subtractor.operator()(temp, mask, 0.01);
+			cv::dilate(mask, mask, kernel);
+		//	cv::erode(mask,mask, kernel);
 			
-			//	computer similarity with corresponding feature in the previous frame or in the first frame
-			//find more features to track
-			//draw bounding rects
-			for(int i=0; i< currPoints.size(); i++)
-				circle(currFrame, currPoints[i], 3, cv::Scalar(0,255,0));
+			
+			cv::imshow("asdfasddddddf", mask);
+			
+			cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+			
+			int amount = 0;
+			//find points within a countour
+			for(size_t i = 0; i < contours.size(); i++)
+			{
+				for(size_t j = 0; j < currPoints.size(); j++)
+				{
+					if(cv::pointPolygonTest(contours[i], currPoints[j], false) >= 0)
+					{
+						amount++;
+						if(amount > 2)
+						{
+							goodContours.push_back(contours[i]);
+							amount = 0;
+							j = currPoints.size();
+						}
+					}
+				}
+			}
+			
+			
+			//draw rectangles
+			for(size_t i = 0; i < goodContours.size(); i++)
+			{
+				cv::Moments moment = moments((cv::Mat)goodContours[i]);
+				double area = moment.m00;
 
-			prevFrame = currFrame.clone();
-			prevGray = currGray.clone();
+					//std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+					//cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
+					boundRect = cv::boundingRect(goodContours[i]);
+					cv::rectangle(currFrame, boundRect.tl(), boundRect.br(),  cv::Scalar(255,0,0), 2);	
+				
+			}
+			
+				
+			for(size_t i=0; i< currPoints.size(); i++)
+				if(status[i] !=0 )
+				//	circle(currFrame, currPoints[i], 3, cv::Scalar(0,255,0));
+
+
+			//TODO: add detection for non moving people
+			//TODO: add detection of features only around current objects
+			
 			prevPoints = currPoints;
-
 			cv::imshow("Result", currFrame);
 			cv::waitKey(1);
-		}
+		}	
 	}
 }
-
-
