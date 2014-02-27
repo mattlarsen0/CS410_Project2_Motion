b8385b9217393546ef6668a97df9beaada87b430
mattlarsen0 <matt.larsen0@gmail.com>
2014-02-26 20:16:41 -0800

    everything working. need to fix exception and remove old points

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@ int main()
 		cv::VideoCapture vid;
 		std::vector<cv::Point2f> prevPoints;
 		std::vector<cv::Point2f> currPoints;
+		std::vector<cv::Point2f> newPoints;
+		cv::Mat roi;
+		std::vector<std::vector<cv::Point2f>> groupedPoints;
 		std::vector<cv::Point2f> searchPoints;
 		cv::Mat background;
 		std::vector<cv::Point2f> topPoints;
@@ -48,57 +51,52 @@ int main()
 	    std::vector<float> err;
 		
 		cv::TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03);
-		cv::BackgroundSubtractorMOG2 subtractor(50, 12, false);
+		cv::BackgroundSubtractorMOG2 subtractor(60, 3, false);
 		
 		vid.open("test2.avi");
 
+		std::cout << "Learning background...\n";
+		//learn the background
+		for(int i = 0; i < 120; i ++)
+		{
+			vid.read(currFrame);
+			cv::cvtColor(currFrame, currFrame, CV_RGB2GRAY);	
+			cv::blur(currFrame, currFrame, cv::Size(9,9));
+			subtractor.operator()(currFrame, mask,0.1);
+		}
+		std::cout << "Finished background learning, init first frame...\n";
 
 		//do the first frame
-		vid.read(temp);
-
-		cv::blur(temp, temp, cv::Size(9,9));
-		subtractor.operator()(temp, mask);
-
 		vid.read(prevFrame);
 
 		cv::cvtColor(prevFrame, prevFrame, CV_RGB2GRAY);	
 		cv::blur(prevFrame, prevFrame, cv::Size(9,9));
-		int frame=120;
-		
+		cv::goodFeaturesToTrack(prevFrame, prevPoints, 500, 0.01, 3);
+		cv::cornerSubPix(prevFrame, prevPoints, winSize, zeroZone, termcrit);
+	
 		while(vid.read(currFrame) == true)
 		{
 			temp = currFrame.clone();
-
+			
 			cv::cvtColor(temp, temp, CV_RGB2GRAY);	
 			cv::blur(temp, temp, cv::Size(9,9));
-
-			if(frame == 120)
-			{
-				cv::goodFeaturesToTrack(prevFrame, prevPoints, 1000, 0.01, 3);
-
-				cv::cornerSubPix(prevFrame, prevPoints, winSize, zeroZone, termcrit);
-				frame = 0;
-			}
-
-			frame++;
+			
 
 			//create optical flow pyramid
 			cv::calcOpticalFlowPyrLK(prevFrame, temp, prevPoints, currPoints, status, err, winSize, 3, termcrit);
-
 			prevFrame = temp.clone();
-			cv::Mat kernel = cv::Mat::ones(3,3, CV_32F);
+			
+			//subtract background to get objects
+			subtractor.operator()(temp, mask, 0);
+			cv::erode(mask,mask, cv::Mat());
+			cv::dilate(mask, mask, cv::Mat());
+			cv::imshow("Background Mask", mask);
 
-			//find and draw rectangles around movement
-			subtractor.operator()(temp, mask, 0.01);
-			cv::dilate(mask, mask, kernel);
-		//	cv::erode(mask,mask, kernel);
-			
-			
-			cv::imshow("asdfasddddddf", mask);
 			
 			cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-			
+
 			int amount = 0;
+
 			//find points within a countour
 			for(size_t i = 0; i < contours.size(); i++)
 			{
@@ -107,7 +105,7 @@ int main()
 					if(cv::pointPolygonTest(contours[i], currPoints[j], false) >= 0)
 					{
 						amount++;
-						if(amount > 2)
+						if(amount > 1)
 						{
 							goodContours.push_back(contours[i]);
 							amount = 0;
@@ -117,29 +115,43 @@ int main()
 				}
 			}
 			
-			
-			//draw rectangles
+			for(size_t i=0; i< currPoints.size(); i++)
+			{	
+				if(status[i] !=0 )
+			{	circle(currFrame, currPoints[i], 3, cv::Scalar(0,255,0));}
+			}
+
+			//draw rectangles and find new points
 			for(size_t i = 0; i < goodContours.size(); i++)
 			{
-				cv::Moments moment = moments((cv::Mat)goodContours[i]);
-				double area = moment.m00;
+				boundRect = cv::boundingRect(goodContours[i]);
+			
+				//create mask from rectangle and search for points
+				cv::Mat featureMask = cv::Mat::zeros(temp.size(), CV_8U);
+				//TODO: CATCH AND FIND EXCEPTION
+				//TODO: DELETE OLD POINTS
+				try
+				{
+					roi = cv::Mat(featureMask, cv::Rect(boundRect.x, boundRect.y, boundRect.width + 10, boundRect.height + 10));
+				}
+				catch(int exception) //near edge
+				{
+					roi = cv::Mat(featureMask, cv::Rect(boundRect.x, boundRect.y, boundRect.width, boundRect.height));
+				}
 
-					//std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
-					//cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
-					boundRect = cv::boundingRect(goodContours[i]);
-					cv::rectangle(currFrame, boundRect.tl(), boundRect.br(),  cv::Scalar(255,0,0), 2);	
-				
+				roi = cv::Scalar(255, 255, 255);
+				cv::goodFeaturesToTrack(temp, newPoints, 3, 0.01, 3, featureMask);
+
+				for(size_t j = 0; j < newPoints.size(); j++)
+				{
+					currPoints.push_back(newPoints[j]);
+				}
+
+				newPoints.clear();
+				cv::rectangle(currFrame, boundRect.tl(), boundRect.br(),  cv::Scalar(255,0,0), 2);	
 			}
-			
-				
-			for(size_t i=0; i< currPoints.size(); i++)
-				if(status[i] !=0 )
-				//	circle(currFrame, currPoints[i], 3, cv::Scalar(0,255,0));
 
-
-			//TODO: add detection for non moving people
-			//TODO: add detection of features only around current objects
-			
+			goodContours.clear();
 			prevPoints = currPoints;
 			cv::imshow("Result", currFrame);
 			cv::waitKey(1);
